fb6722081262c011ab12eb76df661fee9d51d525
bagget777 <123551503+bagget777@users.noreply.github.com>
2023-07-24 00:15:33 +0600

    hm_3

--- posts/tests.py
+++ posts/tests.py
@@ -1,6 +1,6 @@
+
 from django.test import TestCase, Client
 from django.urls import reverse
-
 
 class PostsTestCase(TestCase):
     def setUp(self):
@@ -8,16 +8,15 @@ class PostsTestCase(TestCase):
 
     def test_index_view(self):
         response = self.client.get(reverse("index-page")) # reverse вернет http://127.0.0.1:8000/
-
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "posts/index.html")
 
     def test_contacts_view(self):
         response = self.client.get(reverse("contacts-page"))
-
         self.assertEqual(response.status_code, 200)
-
+        self.assertTemplateUsed(response, "posts/contacts.html")
+        
     def test_about_view(self):
         response = self.client.get(reverse("about-page"))
-
         self.assertEqual(response.status_code, 200)
-        
+        self.assertTemplateUsed(response, "posts/about.html")
